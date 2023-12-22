021d080b775836c45da59369357fbbacdd456e95
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2023-12-22 11:04:32 +0530

    new commit

--- src/com/company/bs_2.java
+++ src/com/company/bs_2.java
@@ -3,22 +3,22 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class bs_2 {
     static void position(int []arr,int key){
         int start=-1,end=-1,l=0,r=arr.length-1;
         int mid=l+(r-l)/2;
 
         while(l<r){
-           if(arr[mid]==key){
-               start=mid;
-               end=mid;
-               break;
-           }
-           else if (arr[mid]>key){
-               r=mid-1;
-           }else if(arr[mid]<key){
-               l=mid+1;
-           }
+            if(arr[mid]==key){
+                start=mid;
+                end=mid;
+                break;
+            }
+            else if (arr[mid]>key){
+                r=mid-1;
+            }else if(arr[mid]<key){
+                l=mid+1;
+            }
         }
 
         while(start>0 && arr[mid-1]==key){
@@ -32,7 +32,7 @@ public class Main {
         System.out.println(start);
         System.out.println(end);
 
-        
+
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
